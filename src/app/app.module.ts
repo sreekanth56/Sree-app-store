8a1bbb8cbdade00530eca2227b13060bc9eec1c7
sreekanth56 <koye.sree@gmail.com>
2020-03-14 11:05:20 +0530

    dropdown custom dritive added and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeItemComponent,
     ShoppingEditComponent,
     ShoppingListComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule,
