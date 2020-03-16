50bd3bc58b284d9d65a81b771d4b908d78fc282e
sreekanth56 <koye.sree@gmail.com>
2020-03-16 10:11:22 +0530

    Manage Recipes to recipe services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from 'src/app/shared/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,16 +12,13 @@ export class RecipeListComponent implements OnInit {
 
  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
+  recipes: Recipe[] = [];
 
-    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6hxsP6_EzISOAE_dLoA1Ld5kMDFvU7QVxJUJUBpuFjYFou7GO'),
-
-    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1Dp-9pCpQdsXxkw3wjUk5ep8ii1hFzARuuGFItD3JsQjWaxo')
-  ]
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+
+    this.recipes = this.recipeService.getRecipe();
   }
 
 
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from '../shared/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,15 @@ import { Injectable } from '@angular/core';
 export class RecipeService {
 
   constructor() { }
+
+  recipes: Recipe[] = [
+
+    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6hxsP6_EzISOAE_dLoA1Ld5kMDFvU7QVxJUJUBpuFjYFou7GO'),
+
+    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1Dp-9pCpQdsXxkw3wjUk5ep8ii1hFzARuuGFItD3JsQjWaxo')
+  ];
+
+  getRecipe(){
+    return this.recipes.slice();
+  }
 }
