fb95567b62deed35dc69427da716f2a3a795f2c8
sreekanth56 <koye.sree@gmail.com>
2020-03-16 11:06:46 +0530

    adding ingredients to recipe

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../shared/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,25 @@ recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
 
-    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6hxsP6_EzISOAE_dLoA1Ld5kMDFvU7QVxJUJUBpuFjYFou7GO'),
+    new Recipe(
+      'test recipe',
+      ' a simple test recipe',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6hxsP6_EzISOAE_dLoA1Ld5kMDFvU7QVxJUJUBpuFjYFou7GO',
+      [
+        new Ingredient('meat',1),
+        new Ingredient('French fries', 20)
+      ]
+      ),
 
-    new Recipe('test recipe',' a simple test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1Dp-9pCpQdsXxkw3wjUk5ep8ii1hFzARuuGFItD3JsQjWaxo')
+    new Recipe(
+      'test recipe',
+      ' a simple test recipe',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1Dp-9pCpQdsXxkw3wjUk5ep8ii1hFzARuuGFItD3JsQjWaxo',
+      [
+        new Ingredient('Bunts',2),
+        new Ingredient('Meat', 2)
+      ]
+      )
   ];
 
   getRecipe(){
